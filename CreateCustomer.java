5d6ceb303f4ea899a2a34e5603d26cee27c4e8e9
Kyrie Cerezo <cerezokyrie@gmail.com>
2017-12-08 16:52:38 +0800

    Add files via upload
    initial working program for creating customer

--- CreateCustomer.java
+++ CreateCustomer.java
@@ -2,23 +2,42 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+ 
 package createcustomer;
 import java.sql.*;
+import java.util.*;
 /**
  *
  * @author OJOJ YNEL DELA CRUZ
  */
 public class CreateCustomer {
-       private static final String USERNAME= "root";
-       private static final String PASSWORD= "";
-       private static final String path = "jdbc:mysql://localhost/order_system";
+ //      private static final String USERNAME= "root";
+    //   private static final String PASSWORD= "";
+       
     public static void main(String[] args) {
+   
+        Scanner kbd = new Scanner (System.in);
+        System.out.println("Enter your Last Name: ");
+        String lastN = kbd.nextLine();
+        System.out.println("Enter your First Name: ");
+        String firstN = kbd.nextLine();
+        System.out.println("Enter your Address: ");
+        String adds = kbd.nextLine();
+        System.out.println("Enter your Contact Number: ");
+        int contact = kbd.nextInt();
+        
         try {
-            Connection conn = DriverManager.getConnection(path,USERNAME,PASSWORD);
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/order_system?autoReconnect=true&useSSL=false", "root","");      
+            Statement st = conn.createStatement ();
+            String query = "insert into customer" + "values('"+lastN+"','"+firstN+"','"+adds+"'.'"+contact+");";
+            st.executeUpdate(query);
             
+     
         } catch (SQLException e){
         }  
+       
+      
     }
     
 }
+
