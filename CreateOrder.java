23e88d1e04931d6cda4989b736babdbc6e7e6c8f
Ojoj <34259712+OjojYnel@users.noreply.github.com>
2017-12-08 18:28:15 +0800

    initial program for creating orders

---
+++ CreateOrder.java
@@ -0,0 +1,27 @@
+package createOrder;
+import java.sql.*;
+import java.util.*;
+
+public class CreateOrder {
+    public static void main(String[] args) {
+   
+        Scanner kbd = new Scanner (System.in);
+            System.out.println("Enter Date: ");
+            String order_date = kbd.nextLine();
+            System.out.println("Enter Product Code: ");
+            String product_code = kbd.nextLine();
+        
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/order_item?autoReconnect=true&useSSL=false", "root","");      
+            Statement st = conn.createStatement ();
+            String query = "insert into customer" + "values('"+order_date+"','"+product_code+");";
+            st.executeUpdate(query);
+            
+     
+        } catch (SQLException e){
+        } 
+       
+      
+    }
+    
+}
